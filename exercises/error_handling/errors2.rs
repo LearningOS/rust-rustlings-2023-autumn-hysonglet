762a7b221d2c9636d35eb6cd6383b3f11353ecc0
hunter <hunter@technocreatives.com>
2023-10-15 16:50:00 +0800

    53

--- exercises/error_handling/errors2.rs
+++ exercises/error_handling/errors2.rs
@@ -19,16 +19,22 @@
 // Execute `rustlings hint errors2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::num::ParseIntError;
 
-pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
+pub fn total_cost(item_quantity: &str) -> Result<i32, String> {
     let processing_fee = 1;
     let cost_per_item = 5;
-    let qty = item_quantity.parse::<i32>();
+    let qty: Result<i32, ParseIntError> = item_quantity.parse::<i32>();
 
-    Ok(qty * cost_per_item + processing_fee)
+    if qty.is_err(){
+        Err("invalid digit found in string".into())
+        // Err(qty.into_err().to_string())
+    }
+    else{
+        let qty = qty.unwrap();
+        Ok(qty * cost_per_item + processing_fee)
+    }
 }
 
 #[cfg(test)]

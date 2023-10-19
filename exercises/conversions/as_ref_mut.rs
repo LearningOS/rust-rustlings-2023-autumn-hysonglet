70c719c49ec2671399c46e7615a0bf2b413ec58f
hunter <hunter@technocreatives.com>
2023-10-19 21:39:08 +0800

    100

--- exercises/conversions/as_ref_mut.rs
+++ exercises/conversions/as_ref_mut.rs
@@ -7,25 +7,44 @@
 // Execute `rustlings hint as_ref_mut` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
+use std::{str::Bytes, ops::DerefMut};
 
 // Obtain the number of bytes (not characters) in the given argument.
 // TODO: Add the AsRef trait appropriately as a trait bound.
-fn byte_counter<T: AsRef<ByteBuffer>>(arg: T) -> usize {
+fn byte_counter<T>(arg: T) -> usize 
+where
+    T: AsRef<str>,
+{
     arg.as_ref().as_bytes().len()
 }
 
 // Obtain the number of characters (not bytes) in the given argument.
 // TODO: Add the AsRef trait appropriately as a trait bound.
-fn char_counter<T>(arg: T) -> usize {
+fn char_counter<T>(arg: T) -> usize 
+where
+    T: AsRef<str>,
+{
     arg.as_ref().chars().count()
 }
 
 // Squares a number using as_mut().
 // TODO: Add the appropriate trait bound.
-fn num_sq<T: ?Sized>(arg: &mut T) {
-    // TODO: Implement the function body.
-    *arg = (*arg as u32).pow(2) as T;
+// fn num_sq<T:AsMut<T> + std::ops::Mul<Output = T>>(arg: &mut T)
+// {
+//     // TODO: Implement the function body.
+//     // *arg = (*arg as u32).pow(2) as T;
+//     let x = *arg;
+//     let y = x * x;
+//     // *arg = (*arg) * (*arg);
+// }
+// use std::ops::Deref;
+// fn num_sq<T>(arg: &mut T) 
+// {
+
+// }
+fn num_sq(arg: &mut Box<u32>) {
+    *arg = Box::new(**arg * **arg);
 }
 
 #[cfg(test)]
